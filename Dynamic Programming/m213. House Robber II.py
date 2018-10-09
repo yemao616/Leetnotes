6d89acac9f532e988e68735e7c792bb13d9a1b09
Ye Mao <ymao4@ncsu.edu>
2018-10-09 00:28:42 -0400

    10.08 update

---
+++ Dynamic Programming/m213. House Robber II.py
@@ -0,0 +1,39 @@
+
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+# Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+# Example 1:
+
+# Input: [2,3,2]
+# Output: 3
+# Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+#              because they are adjacent houses.
+# Example 2:
+
+# Input: [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+#              Total amount you can rob = 1 + 3 = 4.
+
+
+class Solution(object):		# O(N), solution based on robber (#198)
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n < 3:
+            return max(nums)
+        return max(self.helper(nums, 0, n-1), self.helper(nums, 1, n))
+        
+    def helper(self, nums, start, end):
+
+        a, b = nums[start], max(nums[start], nums[start+1])
+        for i in xrange(start+2, end):
+            a, b = b, max(a+nums[i], b)
+
+        return max(a, b)
